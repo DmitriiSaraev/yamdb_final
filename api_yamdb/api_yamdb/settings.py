b389c37cf9ad017c3c43315999f3486d4eee3399
unknown <saraevdmitrii@gmail.com>
2023-03-26 09:32:50 +0500

    com

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('HOSTS', default='*')
 
 
 # Application definition
