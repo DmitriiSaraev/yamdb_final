a7c52f134e477c79db7b016e31a88ed4b842ab73
Дмитрий Сараев <saraevdmitrii@gmail.com>
2023-03-30 11:23:51 +0500

    com

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.25.35', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['158.160.1.108', '127.0.0.1', 'localhost']
 
 
 # Application definition
